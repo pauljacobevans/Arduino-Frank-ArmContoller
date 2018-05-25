1d09c98e7758d0b47c4ec49c2e313088062f08c3
PJ Evans <pj.evans@hcminst.com>
2018-05-24 20:55:41 -0700

    Added door as a separate run apart from the other servos... moves now deal with 3 instead of 4.

--- Arduino-Frank-ArmController/Arduino-Frank-ArmController/Arduino-Frank-ArmController.ino
+++ Arduino-Frank-ArmController/Arduino-Frank-ArmController/Arduino-Frank-ArmController.ino
@@ -29,7 +29,7 @@ const int servo[4] = { 5, 6, 9, 10 }; // servoDoor ;;; servoBicep  ;;; servoTric
 
 // Servo Final Position
 
-const int attackArm[4] = { 1500, 1000, 600, 1200 }; // servoDoor, servoBicep, servoTricep, servoWrist 'default saved' position
+const int attackArm[3] = { 1000, 600, 1200 }; // servoBicep, servoTricep, servoWrist 'default saved' position
 
 // Servo Speed Changes
 
@@ -59,6 +59,7 @@ Servo servoWrist;
 int statusArm = 0;
 int rcPrep = 0;
 int rcFire = 0;
+int door = 0;
 int armThere[2] = { 0, 0 };
 
 void setup() {
@@ -86,24 +87,38 @@ void loop() {
 			servoWrist.attach(servo[3]);
 			if (slowMove[0])
 			{
-				moveStep(engageArm[0], engageArm[1], engageArm[2], engageArm[3], 1);
+				moveStep(engageArm[1], engageArm[2], engageArm[3], 1);
 			}
 			else
 			{
-				moveNow(engageArm[0], engageArm[1], engageArm[2], engageArm[3], 1);
+				moveNow(engageArm[1], engageArm[2], engageArm[3], 1);
+			}
+			servoDoor.writeMicroseconds(engageArm[0]);
+			door = pulseIn(servo[0], HIGH, 90000);
+			while (door != engageArm[0])
+			{
+				delay(5);
+				door = pulseIn(servo[0], HIGH, 90000);
 			}
 		}
 		break;
 	case 2:
 		if (armThere[0] != 2 || armThere[1] = 0)
 		{
+			servoDoor.writeMicroseconds(deployArm[0]);
+			door = pulseIn(servo[0], HIGH, 90000);
+			while (door != deployArm[0])
+			{
+				delay(5);
+				door = pulseIn(servo[0], HIGH, 90000);
+			}
 			if (slowMove[1])
 			{
-				moveStep(deployArm[0], deployArm[1], deployArm[2], deployArm[3], 2);
+				moveStep(deployArm[1], deployArm[2], deployArm[3], 2);
 			}
 			else
 			{
-				moveNow(deployArm[0], deployArm[1], deployArm[2], deployArm[3], 2);
+				moveNow(deployArm[1], deployArm[2], deployArm[3], 2);
 			}
 		}
 		break;
@@ -112,11 +127,11 @@ void loop() {
 		{
 			if (slowMove[2])
 			{
-				moveStep(attackArm[0], attackArm[1], attackArm[2], attackArm[3], 3);
+				moveStep(attackArm[0], attackArm[1], attackArm[2], 3);
 			}
 			else
 			{
-				moveNow(attackArm[0], attackArm[1], attackArm[2], attackArm[3], 3);
+				moveNow(attackArm[0], attackArm[1], attackArm[2], 3);
 			}
 		{
 		break;
@@ -147,32 +162,31 @@ void loop() {
 
 }
 
-void moveStep(int w, int x, int y, int z, int v) {
+void moveStep(int x, int y, int z, int v) {
 	armThere[0] = v;
 	armThere[1] = 0;
-	int armNew[4] = { w, x, y, z };
-	int armCurrent[4];
-	int armTotal[4];
-	float armStep[4];
+	int armNew[3] = { x, y, z };
+	int armCurrent[3];
+	int armTotal[3];
+	float armStep[3];
 	int rcCheck[2];
 
-	for (int i = 0; i < 4; i++)
+	for (int i = 0; i < 3; i++)
 	{
-		armCurrent[i] = pulseIn(servo[i], HIGH, 90000);
+		armCurrent[i] = pulseIn(servo[i + 1], HIGH, 90000);
 		armTotal[i] = armNew[i] - armCurrent[i];
 		armStep[i] = float(armTotal[i]) / stepsServo;
 	}
 
 	for (int j = 0; j < stepsServo; j++)
 	{
-		for (int k = 0; k < 4; k++)
+		for (int k = 0; k < 3; k++)
 		{
 			armCurrent[k] = armCurrent[k] + armStep[k];
 		}
-		servoDoor.writeMicroseconds(armCurrent[0]);
-		servoBicep.writeMicroseconds(armCurrent[1]);
-		servoTricep.writeMicroseconds(armCurrent[2]);
-		servoWrist.writeMicroseconds(armCurrent[3]);
+		servoBicep.writeMicroseconds(armCurrent[0]);
+		servoTricep.writeMicroseconds(armCurrent[1]);
+		servoWrist.writeMicroseconds(armCurrent[2]);
 
 		delay(delayServo);
 
@@ -202,17 +216,15 @@ void moveStep(int w, int x, int y, int z, int v) {
 		}
 	}
 
-	servoDoor.writeMicroseconds(w);
 	servoBicep.writeMicroseconds(x);
 	servoTricep.writeMicroseconds(y);
 	servoWrist.writeMicroseconds(z);
 	armThere[1] = 1;
 }
 
-void moveNow(int w, int x, int y, int z, int v) {
+void moveNow(int x, int y, int z, int v) {
 	armThere[0] = v;
 	armThere[1] = 0;
-	servoDoor.writeMicroseconds(w);
 	servoBicep.writeMicroseconds(x);
 	servoTricep.writeMicroseconds(y);
 	servoWrist.writeMicroseconds(z);
