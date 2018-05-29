82c797d786c052ebb681af58a98dcc26599fafae
Paul Jacob Evans <pjevans@number3productions.com>
2018-05-28 17:35:50 -0700

    Removed readMicroseconds as that didn't actually read the current position, just wht was last written to the servo.

--- Arduino-Frank-ArmController/Arduino-Frank-ArmController/Arduino-Frank-ArmController.ino
+++ Arduino-Frank-ArmController/Arduino-Frank-ArmController/Arduino-Frank-ArmController.ino
@@ -30,7 +30,7 @@ const int servo[4] = { 5, 6, 9, 10 }; // servoDoor ;;; servoBicep  ;;; servoTric
 
 // Servo Final Position
 
-const int attackArm[3] = { 1000, 600, 1200 }; // servoBicep, servoTricep, servoWrist 'default saved' position
+const int attackArm[3] = { 1100, 600, 1275 }; // servoBicep, servoTricep, servoWrist 'default saved' position
 
 // Servo Speed Changes
 
@@ -43,8 +43,8 @@ const int stepsServo = 20;  // servoBicep, servoTricep, servoWrist # of steps to
 // == Shouldn't have to edit anything under this line!
 // =================================================================
 
-const int engageArm[4] = { 1500, 1950, 975, 2100 }; // servoDoor, servoBicep, servoTricep, servoWrist 'engaged' position
-const int deployArm[4] = { 1500, 1600, 1100, 800 }; // servoDoor, servoBicep, servoTricep, servoWrist 'deployed' position
+const int engageArm[4] = { 2260, 1965, 1050, 2100 }; // servoDoor, servoBicep, servoTricep, servoWrist 'engaged' position
+const int deployArm[4] = { 825, 1370, 1100, 825 }; // servoDoor, servoBicep, servoTricep, servoWrist 'deployed' position
 
 // =================================================================
 // == END OF Varible Declarations
@@ -58,13 +58,9 @@ Servo servoTricep;
 Servo servoWrist;
 
 int statusArm = 0;
-bool doorOpen = false;
+int doorCurrent = 1502;
 int rcPrep = 0;
 int rcFire = 0;
-int door = 0;
-int bicep = 0;
-int tricep = 0;
-int armThere[2] = { 0, 0 };
 
 void setup() {
 	Serial.begin(9600);
@@ -83,69 +79,54 @@ void loop() {
 		servoWrist.detach();
 		break;
 	case 1:
-		if (armThere[0] != 1 || armThere[1] = 0)
+		servoDoor.attach(servo[0]);
+		servoBicep.attach(servo[1]);
+		servoTricep.attach(servo[2]);
+		servoWrist.attach(servo[3]);
+		if (slowMove[0])
 		{
-			servoDoor.attach(servo[0]);
-			servoBicep.attach(servo[1]);
-			servoTricep.attach(servo[2]);
-			servoWrist.attach(servo[3]);
-			if (slowMove[0])
-			{
-				moveStep(engageArm[1], engageArm[2], engageArm[3], 1);
-			}
-			else
-			{
-				moveNow(engageArm[1], engageArm[2], engageArm[3], 1);
-			}
-			bicep = servoBicep.readMicroseconds(engageArm[1]);
-			tricep = servoBicep.readMicroseconds(engageArm[2]);
-			if (bicep >= engageArm[1] && tricep <= engageArm[2])
-			{
-				servoDoor.writeMicroseconds(engageArm[0]);
-				door = servoDoor.readMicroseconds;
-				if (door <= engageArm[0])
-				{
-					doorOpen = false;
-					armThere[1] = 1;
-				}
-			}
+			moveStep(engageArm[1], engageArm[2], engageArm[3]);
 		}
+		else
+		{
+			moveNow(engageArm[1], engageArm[2], engageArm[3]);
+		}
+		if (doorCurrent < engageArm[0])
+    {
+      delay(300);
+		  servoDoor.writeMicroseconds(engageArm[0]);
+    }
 		break;
 	case 2:
-		if (armThere[0] != 2 || armThere[1] = 0)
-		{
+		if (doorCurrent > deployArm[0])
+    {
 			servoDoor.writeMicroseconds(deployArm[0]);
-			door = servoDoor.readMicroseconds;
-			if (door >= deployArm[0])
-			{
-				doorOpen = true;
-				if (slowMove[1])
-				{
-					moveStep(deployArm[1], deployArm[2], deployArm[3], 2);
-				}
-				else
-				{
-					moveNow(deployArm[1], deployArm[2], deployArm[3], 2);
-				}
-				armThere[1] = 1;
-			}
+      delay(11000);
+    }
+		if (slowMove[1])
+		{
+			moveStep(deployArm[1], deployArm[2], deployArm[3]);
+		}
+		else
+		{
+			moveNow(deployArm[1], deployArm[2], deployArm[3]);
 		}
 		break;
 	case 3:
-		if (armThere[0] != 3 || armThere[1] = 0)
+		if (slowMove[2])
 		{
-			if (slowMove[2])
-			{
-				moveStep(attackArm[0], attackArm[1], attackArm[2], 3);
-			}
-			else
-			{
-				moveNow(attackArm[0], attackArm[1], attackArm[2], 3);
-			}
-			armThere[1] = 1;
+			moveStep(attackArm[0], attackArm[1], attackArm[2]);
+		}
+		else
 		{
+			moveNow(attackArm[0], attackArm[1], attackArm[2]);
+		}
 		break;
 	}
+
+ Serial.println(servoDoor.readMicroseconds());
+
+ doorCurrent = servoDoor.readMicroseconds();
 
 	rcPrep = pulseIn(rcPin[0], HIGH, 90000);
 	rcFire = pulseIn(rcPin[1], HIGH, 90000);
@@ -172,9 +153,7 @@ void loop() {
 
 }
 
-void moveStep(int x, int y, int z, int v) {
-	armThere[0] = v;
-	armThere[1] = 0;
+void moveStep(int x, int y, int z) {
 	int armNew[3] = { x, y, z };
 	int armCurrent[3];
 	int armTotal[3];
@@ -194,8 +173,8 @@ void moveStep(int x, int y, int z, int v) {
 		{
 			armCurrent[k] = armCurrent[k] + armStep[k];
 		}
+    servoTricep.writeMicroseconds(armCurrent[1]);
 		servoBicep.writeMicroseconds(armCurrent[0]);
-		servoTricep.writeMicroseconds(armCurrent[1]);
 		servoWrist.writeMicroseconds(armCurrent[2]);
 
 		delay(delayServo);
@@ -226,15 +205,13 @@ void moveStep(int x, int y, int z, int v) {
 		}
 	}
 
+  servoTricep.writeMicroseconds(y);
 	servoBicep.writeMicroseconds(x);
-	servoTricep.writeMicroseconds(y);
 	servoWrist.writeMicroseconds(z);
 }
 
-void moveNow(int x, int y, int z, int v) {
-	armThere[0] = v;
-	armThere[1] = 0;
+void moveNow(int x, int y, int z) {
+  servoTricep.writeMicroseconds(y);
 	servoBicep.writeMicroseconds(x);
-	servoTricep.writeMicroseconds(y);
 	servoWrist.writeMicroseconds(z);
 }
